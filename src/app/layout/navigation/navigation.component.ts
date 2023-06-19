4d3bdf10caaeb935e9ca82d08b5336d826a7a495
prathuziggletech <prathu@ziggletech.com>
2023-06-19 17:53:04 +0530

    json change

--- src/app/layout/navigation/navigation.component.ts
+++ src/app/layout/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation',
@@ -9,12 +10,23 @@ export class NavigationComponent implements OnInit {
 
   urlActive:any;
 
-  constructor(
+  constructor(private router: Router,
   ) { 
   }
 
   ngOnInit(): void {
     
+  }
+
+  reloadPage() {
+    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+    const currentUrl = this.router.url + '?';
+    this.router.navigateByUrl(currentUrl)
+      .then(() => {
+        this.router.navigated = false;
+        this.router.navigate([this.router.url]);
+      });
+      console.log('reload')
   }
 
 }
